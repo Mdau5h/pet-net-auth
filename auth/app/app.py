1acdc077114ab28e04ed24fc412a9f70eeb9771d
mdau5h <adeswepore73@gmail.com>
2024-02-07 00:48:12 +0300

    add views

--- auth/app/app.py
+++ auth/app/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, APIRouter
 # from auth.api.dependencies import token_route
+from auth.api.views import routes
 
 healthcheck_route = APIRouter()
 
 
-@healthcheck_route.get('/v1/health')
+@healthcheck_route.get('/health')
 def health_check():
     return {'status': 'ok'}
 
@@ -12,5 +13,6 @@ def health_check():
 def create_app():
     app = FastAPI(title='pet-net-auth')
     app.include_router(healthcheck_route)
+    app.include_router(routes)
     # app.include_router(token_route, tags=['auth'])
     return app
