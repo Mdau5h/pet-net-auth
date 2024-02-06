204cdc17508e4a46a57b77b86a9197b9ce732934
mdau5h <adeswepore73@gmail.com>
2024-02-07 00:47:21 +0300

    up db on startup

--- asgi.py
+++ asgi.py
@@ -1,9 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
-
 from auth.app.app import create_app
+from auth.database.utils import db_setup
 
 app: FastAPI = create_app()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await db_setup()
 
 
 if __name__ == '__main__':
