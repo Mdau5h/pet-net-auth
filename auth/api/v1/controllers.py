8b5273073ff62ac236dbcdc70034f92f21f91580
mdau5h <adeswepore73@gmail.com>
2024-06-14 20:43:23 +0300

    add message catcher

--- auth/api/v1/controllers.py
+++ auth/api/v1/controllers.py
@@ -1,13 +1,16 @@
 from bcrypt import hashpw, gensalt
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
 from auth.api.v1.db_controllers import create_user_db, get_user_by_login_db
 from auth.app.db import db_transaction
 from auth.serializers.schema import UserRequest
+from auth.utils.enums import EventTypeEnum
 from auth.utils.exceptions import UserExistsError
+from auth.utils.rmq_integration import catch_event
 from config import config
 
 
+@catch_event(EventTypeEnum.CLIENT_REGISTERED)
 @db_transaction
 async def create_user(request_params: UserRequest, session: AsyncSession):
     try:
